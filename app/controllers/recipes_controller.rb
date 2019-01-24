6d160d824a95b94dff7326fcfafdd90a9fc2fb70
Samuel Cornet <samuel@noukod.com>
2019-01-24 22:14:24 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -15,8 +15,7 @@ class RecipesController < ApplicationController
 
   def create
     @recipe = Recipe.create(recipe_params)
-     @recipe.inspect
-    # puts recipes_params.inspect
+   puts recipes_params.inspect
     redirect_to @recipe
   end
 
